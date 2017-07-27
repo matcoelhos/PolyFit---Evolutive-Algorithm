c511630e9ea0950d6b0faab732c033a5146674e0
Mateus Coelho <matcoelhosilva@yahoo.com.br>
2017-07-27 14:47:20 -0300

    Changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -10,46 +10,91 @@
 
 using namespace std;
 
-int popsize = 200;
-int gensize = 5;
+int popsize = 50;
+int gensize = 4;
 int rounds = 100000;
-int percent = 400;
+int percent = 100;
 bool elitism = false;
+
+void testEAFunct ()
+{
+  ifstream readlist;
+  ofstream resultList;
+  vector<string> fileslist;
+  string line;
+
+  readlist.open("files.txt");
+
+  stringstream ss;
+  ss << "EA_tests_" << popsize << "_" << gensize << "_" << rounds << "_" << percent << "_" << elitism << ".txt";
+  resultList.open(ss.str());
+
+  while (getline(readlist,line))
+  {
+  stringstream ss;
+  ss << "Benchmark/" << line;
+      fileslist.push_back(ss.str());
+  }
+
+  readlist.close();
+
+  for (int i = 0; i < fileslist.size(); i++)
+  {
+    int R;
+    PolyFitP p(fileslist[i]);
+    cout << "File: " << fileslist[i] << endl;
+
+    EA algorithm(p, popsize, gensize);
+    R = algorithm.runEA(p, rounds);
+
+    cout << "mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << endl;
+        resultList << "Rounds:" << R << ", mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << endl;
+    resultList << algorithm.population[algorithm.getBest()].disp() << endl;
+    cout << endl << endl;
+  }
+  resultList.close();
+}
+
+void testGAFunct ()
+{
+  ifstream readlist;
+  ofstream resultList;
+  vector<string> fileslist;
+  string line;
+
+  readlist.open("files.txt");
+  stringstream ss;
+  ss << "GA_tests_" << popsize << "_" << gensize << "_" << rounds << "_" << percent << "_" << elitism << ".txt";
+  resultList.open(ss.str());
+
+  while (getline(readlist,line))
+  {
+  stringstream ss;
+  ss << "Benchmark/" << line;
+      fileslist.push_back(ss.str());
+  }
+
+  readlist.close();
+
+  for (int i = 0; i < fileslist.size(); i++)
+  {
+    int R;
+    PolyFitP p(fileslist[i]);
+    cout << "File: " << fileslist[i] << endl;
+
+    GA algorithm(p, popsize, gensize);
+    R = algorithm.runGA(p, rounds, percent, true, elitism);
+
+    cout << "mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << endl;
+        resultList << "Rounds:" << R << ", mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << endl;
+    resultList << algorithm.population[algorithm.getBest()].disp() << endl;
+    cout << endl << endl;
+  }
+  resultList.close();
+}
 
 int main(int argc, char* argv[])
 {
-    ifstream readlist;
-    ofstream resultList;
-    vector<string> fileslist;
-    string line;
-    
-    readlist.open("files.txt");
-    resultList.open("EA_tests_200_5_100000.txt");
-    
-    while (getline(readlist,line))
-    {
-		stringstream ss;
-		ss << "Benchmark/" << line;
-        fileslist.push_back(ss.str());
-    }
-    
-    readlist.close();
-	
-	for (int i = 0; i < fileslist.size(); i++)
-	{
-        int R;
-		PolyFitP p(fileslist[i]);
-		cout << "File: " << fileslist[i] << endl;
-		
-		EA algorithm(p, popsize, gensize);	
-		R = algorithm.runEA(p, rounds);
-		
-		cout << "mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << endl;
-        resultList << "Rounds:" << R << ", mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << endl;
-		resultList << algorithm.population[algorithm.getBest()].disp() << endl;
-		cout << endl << endl;
-	}
-	resultList.close();
+    testGAFunct();
     return 0;
 }
-
