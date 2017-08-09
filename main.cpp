4a890846145fcea1167c2066c42965938232a820
Mateus Coelho <matcoelhosilva@yahoo.com.br>
2017-08-09 16:06:57 -0300

    Main commit

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 int popsize = 50;
 int gensize = 5;
-int rounds = 100000;
+int rounds = 10000000;
 int percent = 400;
 bool elitism = false;
 
@@ -48,7 +48,7 @@ void testEAFunct ()
     R = algorithm.runEA(p, rounds);
 
     cout << "mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << endl;
-        resultList << "Rounds:" << R << ", mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << endl;
+        resultList << "Rounds:" << R << ", mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << ", St. Dev.: " << algorithm.stdev() << endl;
     resultList << algorithm.population[algorithm.getBest()].disp() << endl;
     cout << endl << endl;
   }
@@ -83,10 +83,48 @@ void testGAFunct ()
     cout << "File: " << fileslist[i] << endl;
 
     EA algorithm(p, popsize, gensize);
-    R = algorithm.runGA(p, rounds, percent, true, elitism);
+    R = algorithm.runGA(p, rounds, percent, true, elitism, false);
 
     cout << "mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << endl;
-        resultList << "Rounds:" << R << ", mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << endl;
+        resultList << "Rounds:" << R << ", mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << ", St. Dev.: " << algorithm.stdev() << endl;
+    resultList << algorithm.population[algorithm.getBest()].disp() << endl;
+    cout << endl << endl;
+  }
+  resultList.close();
+}
+
+void testHBDFunct ()
+{
+  ifstream readlist;
+  ofstream resultList;
+  vector<string> fileslist;
+  string line;
+
+  readlist.open("files.txt");
+  stringstream ss;
+  ss << "HBD_tests_" << popsize << "_" << gensize << "_" << rounds << "_" << percent << "_" << elitism << ".txt";
+  resultList.open(ss.str());
+
+  while (getline(readlist,line))
+  {
+    stringstream ss;
+    ss << "Benchmark/" << line;
+    fileslist.push_back(ss.str());
+  }
+
+  readlist.close();
+
+  for (int i = 0; i < fileslist.size(); i++)
+  {
+    int R;
+    PolyFitP p(fileslist[i]);
+    cout << "File: " << fileslist[i] << endl;
+
+    EA algorithm(p, popsize, gensize);
+    R = algorithm.runGA(p, rounds, percent, true, elitism, true);
+
+    cout << "mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << endl;
+        resultList << "Rounds:" << R << ", mean fitness: " << algorithm.getMean() << ", best: " << algorithm.population[algorithm.getBest()].fitness << ", St. Dev.: " << algorithm.stdev() << endl;
     resultList << algorithm.population[algorithm.getBest()].disp() << endl;
     cout << endl << endl;
   }
@@ -95,8 +133,8 @@ void testGAFunct ()
 
 int main(int argc, char* argv[])
 {
-
     testGAFunct();
-    //testEAFunct();    
+    testEAFunct();
+    testHBDFunct();
     return 0;
 }
